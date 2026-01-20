8b1f31783a251ca621e4039ec8498cd682e6886c
devops14498 <paulo_bocardo@estudante.sesisenai.org.br>
2026-01-20 20:59:08 -0300

    Add tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const { greet } = require("./lib/greet");
+const { sum } = require("./lib/sum");
+
 
 const name = process.argv[2];
 
 console.log("=== Git Training CLI ===");
 console.log(greet(name));
 console.log("Dica: edite src/lib/greet.js para mudar a mensagem.");
+console.log("Sum result:", sum(2, 3));
